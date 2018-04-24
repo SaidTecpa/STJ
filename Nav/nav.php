bf1cab159b23fe71f50b64bfe7fcb9e1d39d7b94
Said <said.planck@gmail.com>
2018-04-24 16:40:39 -0500

    Creation of the -nav- and -index-

---
+++ Nav/nav.php
@@ -0,0 +1,10 @@
+<header>
+  <a href="index.php"> <span> STJ </span> </a>
+  <nav>
+    <ul>
+      <li> <a href="index.php"> Inicio </a> </li>
+      <li> <a href="documentacion.php"> Documentación </a> </li>
+      <li> <a href="pyoyectos"> Proyectos </a> </li>
+    </ul>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> Inicio </title>
+    <link rel="stylesheet" href="Style/style.css">
+  </head>
+  <body>
+    <?php require 'Nav/nav.php' ?>
+  </body>
+</html>
