799145baebadeb20838abbb0e4f5a079ee7a7e41
Said <said.planck@gmail.com>
2018-04-24 21:55:50 -0500

    New styles

--- index.php
+++ index.php
@@ -2,13 +2,24 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title> Inicio </title>
+    <title> STJ </title>
     <link rel="stylesheet" href="Style/style.css">
   </head>
   <body>
     <?php require 'Nav/nav.php' ?>
 
+    <div class="ancho-letras">
+      <div class="letras-sliders">
+        <h1>Un mundo de ideas</h1><br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit, sed do eiusmod tempor incididunt ut labore et</p>
+      </div>
+    </div>
     <div id="particles-js"></div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br>
     <script src="particles.js"></script>
     <script src="app.js"></script>
   </body>
