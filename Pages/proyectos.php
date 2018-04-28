5758f974687037b7da73670ac435d564a8fcad51
Said <said.planck@gmail.com>
2018-04-27 20:14:27 -0500

    Changes

--- Pages/proyectos.php
+++ Pages/proyectos.php
@@ -4,30 +4,7 @@
     <meta charset="utf-8">
     <title> Proyectos </title>
     <link rel="stylesheet" href="../Style/style.css">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.card {
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    width: 40%;
-    border-radius: 5px;
-}
-
-.card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
-
-img {
-    border-radius: 5px 5px 0 0;
-}
-
-.container {
-    padding: 2px 16px;
-}
-</style>
-
-
   </head>
   <body>
     <?php require '../Nav/nav1.php' ?>
@@ -35,19 +12,17 @@ img {
       <div class="letra2">
           <h1>Proyectos</h1>
       </div>
-    </div>
+      <div class="do">Texto que va en el div1
+        
+      <div class="du">Texto que va en el div2</div>
+      </div>
 
-    <div class="titulo-doc4">
-
-      <div class="card">
-    <img src="https://pbs.twimg.com/profile_images/558392148697939968/FSHEYq_5_400x400.png" alt="Avatar" style="width:100%">
-    <div class="container">
-      <h4><b>Jane Doe</b></h4><br>
-      <p>Interior Designer</p>
-    </div>
-  </div>
 
     </div>
+
+
+
+
 
   </body>
 </html>
